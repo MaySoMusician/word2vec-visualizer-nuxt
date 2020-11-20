55ef147e42cb4eb9beef7c42f0a9bcf36c515217
MaySoMusician <maysomusician@gmail.com>
2020-11-20 19:06:27 +0900

    Add beatifyMostSimilar function

--- nuxt/userModules/jsonBeautifier/index.ts
+++ nuxt/userModules/jsonBeautifier/index.ts
@@ -17,3 +17,45 @@ export function beatifyVector(vector: number[]): string {
   result += `${currentRow}\n]`
   return result
 }
+
+export function beatifyMostSimilar(
+  similarWordArray: [string, number][]
+): string {
+  const FIXED = 20
+
+  const words = similarWordArray.map((s) => s[0])
+
+  const getLength = (str: string) => {
+    let length = 0
+    for (const s of str) {
+      const char = s.charCodeAt(0)
+      if (
+        (char >= 0x00 && char < 0x81) ||
+        char === 0xf8f0 ||
+        (char >= 0xff61 && char < 0xffa0) ||
+        (char >= 0xf8f1 && char < 0xf8f4)
+      ) {
+        // Half-width
+        length += 1
+      } else {
+        // Full-width
+        length += 2
+      }
+    }
+
+    return length
+  }
+
+  const longestWordLength = Math.max(...words.map((w) => getLength(w)))
+
+  const beautified = similarWordArray
+    .map((input) => {
+      const word = input[0]
+      const diff = longestWordLength - getLength(word)
+      const similarity =
+        input[1] < 0 ? input[1].toFixed(FIXED) : ` ${input[1].toFixed(FIXED)}`
+      return `  [ "${word}", ${' '.repeat(diff)}${similarity} ]`
+    })
+    .join('\n')
+  return '[\n' + beautified + '\n]'
+}
